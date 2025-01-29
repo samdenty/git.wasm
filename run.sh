ec0bb3f0596960ac10cb9dd32db6efdada88da6d
Sam Denty <sam@samdenty.com>
2025-01-29 22:30:46 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
-echo Because it's 40MB, it's treated as static asset and will be fetched from github...
-cat git.wasm > /dev/null;
-echo Done fetching!
-
 echo printing help...
 
-wasm git.wasm -- 'git' 'help';
+wasm node_modules/git.wasm/git.wasm -- 'git' 'help'
 
 echo printing log...
 
-wasm git.wasm --mapdir /repo:. -- 'git' '-C' '/repo' 'log';
+wasm node_modules/git.wasm/git.wasm --mapdir /repo:. -- 'git' '-C' '/repo' 'log'
